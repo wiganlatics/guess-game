43c573c0c06435ca00f437281cf545b121b30c1a
wiganlatics <wiganlatics@users.noreply.github.com>
2017-02-19 10:43:09 +0000

    Add UI components

--- GuessGame/frmGuess.Designer.cs
+++ GuessGame/frmGuess.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Location = new System.Drawing.Point(12, 19);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(302, 17);
+            this.lblInstructions.TabIndex = 3;
+            this.lblInstructions.Text = "Guess the random number between 1 and 100";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(322, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(76, 31);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(12, 52);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(100, 22);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Enabled = false;
+            this.btnGuess.Location = new System.Drawing.Point(322, 52);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(76, 30);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            // 
+            // frmGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(425, 94);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblInstructions);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmGuess";
+            this.Text = "Guess Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnGuess;
     }
 }
 
